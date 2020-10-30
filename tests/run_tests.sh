e2fc0d73bae130c5f9b9654ea45053949870a350
Pavel Rogovoy <progovoy@gmail.com>
2020-10-30 10:25:57 +0200

    add retcodes

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,16 +2,18 @@
 
 CUR_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-${CUR_DIR}/build_vmn_tester.sh ubuntu xenial
+${CUR_DIR}/build_vmn_tester.sh ubuntu xenial || exit 1
 echo "docker run -t -v ${CUR_DIR}/..:${CUR_DIR}/.. vmn_tester:ubuntu_xenial ${CUR_DIR}/run_pytest.sh"
-docker run -t -v ${CUR_DIR}/..:${CUR_DIR}/.. vmn_tester:ubuntu_xenial ${CUR_DIR}/run_pytest.sh
+docker run -t -v ${CUR_DIR}/..:${CUR_DIR}/.. vmn_tester:ubuntu_xenial ${CUR_DIR}/run_pytest.sh || exit 1
 
-${CUR_DIR}/build_vmn_tester.sh ubuntu bionic
+${CUR_DIR}/build_vmn_tester.sh ubuntu bionic || exit 1
 echo "docker run -t -v ${CUR_DIR}/..:${CUR_DIR}/.. vmn_tester:ubuntu_bionic ${CUR_DIR}/run_pytest.sh"
-docker run -t -v ${CUR_DIR}/..:${CUR_DIR}/.. vmn_tester:ubuntu_bionic ${CUR_DIR}/run_pytest.sh
+docker run -t -v ${CUR_DIR}/..:${CUR_DIR}/.. vmn_tester:ubuntu_bionic ${CUR_DIR}/run_pytest.sh || exit 1
 
-${CUR_DIR}/build_vmn_tester.sh ubuntu focal
+${CUR_DIR}/build_vmn_tester.sh ubuntu focal || exit 1
 echo "docker run -t -v ${CUR_DIR}/..:${CUR_DIR}/.. vmn_tester:ubuntu_focal ${CUR_DIR}/run_pytest.sh"
-docker run -t -v ${CUR_DIR}/..:${CUR_DIR}/.. vmn_tester:ubuntu_focal ${CUR_DIR}/run_pytest.sh
+docker run -t -v ${CUR_DIR}/..:${CUR_DIR}/.. vmn_tester:ubuntu_focal ${CUR_DIR}/run_pytest.sh || exit 1
 
-${CUR_DIR}/run_pytest.sh
+${CUR_DIR}/run_pytest.sh || exit 1
+
+exit 0
