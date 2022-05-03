27763b17d1250a8f15ed475148f23033e2261449
ronshilo <ronshilo@gmail.com>
2022-05-03 17:25:35 +0300

    run all tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,15 +2,15 @@
 
 CUR_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-#${CUR_DIR}/build_vmn_tester.sh ubuntu bionic || exit 1
-#echo "docker run --init -t -v ${CUR_DIR}/..:${CUR_DIR}/.. vmn_tester:ubuntu_bionic ${CUR_DIR}/run_pytest.sh"
-#docker run --init -t -v ${CUR_DIR}/..:${CUR_DIR}/.. vmn_tester:ubuntu_bionic ${CUR_DIR}/run_pytest.sh --skip_test test_backward_compatability_with_previous_vmn || exit 1
-#
-#${CUR_DIR}/build_vmn_tester.sh ubuntu focal || exit 1
-#echo "docker run --init -t -v ${CUR_DIR}/..:${CUR_DIR}/.. vmn_tester:ubuntu_focal ${CUR_DIR}/run_pytest.sh"
-#docker run --init -t -v ${CUR_DIR}/..:${CUR_DIR}/.. vmn_tester:ubuntu_focal ${CUR_DIR}/run_pytest.sh --skip_test test_backward_compatability_with_previous_vmn || exit 1
-#
-#${CUR_DIR}/build_previous_vmn_stamper.sh || exit 1
+${CUR_DIR}/build_vmn_tester.sh ubuntu bionic || exit 1
+echo "docker run --init -t -v ${CUR_DIR}/..:${CUR_DIR}/.. vmn_tester:ubuntu_bionic ${CUR_DIR}/run_pytest.sh"
+docker run --init -t -v ${CUR_DIR}/..:${CUR_DIR}/.. vmn_tester:ubuntu_bionic ${CUR_DIR}/run_pytest.sh --skip_test test_backward_compatability_with_previous_vmn || exit 1
+
+${CUR_DIR}/build_vmn_tester.sh ubuntu focal || exit 1
+echo "docker run --init -t -v ${CUR_DIR}/..:${CUR_DIR}/.. vmn_tester:ubuntu_focal ${CUR_DIR}/run_pytest.sh"
+docker run --init -t -v ${CUR_DIR}/..:${CUR_DIR}/.. vmn_tester:ubuntu_focal ${CUR_DIR}/run_pytest.sh --skip_test test_backward_compatability_with_previous_vmn || exit 1
+
+${CUR_DIR}/build_previous_vmn_stamper.sh || exit 1
 
 ${CUR_DIR}/run_pytest.sh || exit 1
 
